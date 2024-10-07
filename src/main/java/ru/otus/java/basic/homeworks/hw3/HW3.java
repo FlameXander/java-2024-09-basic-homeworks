b49473f2a139dc025562e5bde04d71433175de32
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 21:56:43 +0300

    hw3 fix

--- src/main/java/ru/otus/java/basic/homeworks/hw3/HW3.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw3/HW3.java
@@ -3,5 +3,7 @@ package ru.otus.java.basic.homeworks.hw3;
 public class HW3 {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
     }
 }
