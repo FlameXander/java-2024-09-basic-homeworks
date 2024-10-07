6151091217275dd155e3b1d47baba3446bc7f8c7
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-10-07 21:51:26 +0300

    Исправление печати 0

--- src/main/java/ru/otus/java/basic/homeworks/hw3/MainApp.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw3/MainApp.java
@@ -9,7 +9,6 @@ public class MainApp {
     public static void printOneTwoThree() {
         System.out.println(1);
         System.out.println(2);
-        System.out.println(0);
         System.out.println(3);
     }
 
